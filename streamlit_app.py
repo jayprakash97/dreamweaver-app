7f62557495b6a96c7d944b0586dd93cbfd82eb37
Jay Prakash <59421807+jayprakash97@users.noreply.github.com>
2024-09-16 23:18:47 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,39 +1,38 @@
 import streamlit as st
-import pandas as pd
 
-st.set_page_config(
-    page_title="Homepage",
-    page_icon="🏡",
-    layout="centered",
-    initial_sidebar_state="expanded"
-)
+# Give your app a title
+st.title("Your title")
 
-df = pd.DataFrame({"col1": [1, 2, 3],
-                   "col2": [4, 5, 6]})
+# Header
+st.header("Main header")
 
-if "df" not in st.session_state:
-    st.session_state['df'] = df
+# Subheader
+st.subheader("This is a subheader")
 
-if "product" not in st.session_state:
-    st.session_state['product'] = 0
+# Markdown
+st.markdown("This is markdown **text**")
+st.markdown("# Header1")
+st.markdown("## Header 2")
+st.markdown("### Header 3")
 
+# Caption
+st.caption("This is a caption")
 
-def multiply(x1, x2):
-    st.session_state["product"] = x1 * x2
+# Code block
+st.code("""import pandas as pd
+pd.read_csv(my_csv_file)
+""")
 
+# Preformatted text
+st.text("Some text")
 
-if __name__ == "__main__":
+# LaTeX
+st.latex("x = 2^2")
 
-    st.title("Homepage")
+# Divider
+st.text('Text above divider')
+st.divider()
+st.text('Text below divider')
 
-    col1, col2 = st.columns(2)
-
-    with col1:
-        x1 = st.number_input("Pick a number", 0, 10, key="x1")
-    with col2:
-        x2 = st.number_input("Pick another number", 0, 10, key='x2')
-
-    st.button("Multiply!", type="primary", on_click=multiply, args=((x1, x2)))
-
-    st.write(st.session_state['df'])
-    st.write(st.session_state)
+#st.write
+st.write('Some text')
