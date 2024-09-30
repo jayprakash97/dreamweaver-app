6a2774f2a506c0bb3ce3286c79fe20251cabfc43
Jay Prakash <59421807+jayprakash97@users.noreply.github.com>
2024-09-29 17:29:17 -0700

    Update 03_👩‍🏫_Contact.py

--- pages/03_👩‍🏫_Contact.py
+++ pages/03_👩‍🏫_Contact.py
@@ -1,3 +1,17 @@
 import streamlit as st
 
 st.title("Contact with WonderScriber Team")
+
+with st.form("form_key"):
+    st.write("Craft personalized stories that bring adventure to life and ignite imagination and creativity")
+    Department = st.selectbox("Department", options=["Sales", "Human Resources", "Information Technology", "Public Relation", "Complaince", "Marketing", "Data Engineering", "Data Science", "Data Analytics"])
+    main_character = st.text_input("What will be the name of the main character?")
+    audience = st.selectbox("Audience", options=["children", "young adult", "adult", "senior"])
+    story_setting = st.selectbox("Story Setthing", options=["Magical Kingdoms", "Underwater Kingdoms", "Pirate ships", "Exotic locations", "Imaginary world", "Digital words", "Others"])
+    story_type = st.selectbox("Story Type", options=["Fantacy", "Fairy Tales", "Mythology", "Bedtime stories", "Adventure", "Mystery", "Love", "Horror", ])
+    story_theme = st.text_input("What would be topic of the story?", placeholder="Leave brief idea of a story")
+    moral_lesson = st.text_input("What would be the moral of this story?")
+    story_length = st.selectbox("Story Length (in words) ", options=["300", "400", "500"])
+    
+    submit_btn = st.form_submit_button("Submit")
+
