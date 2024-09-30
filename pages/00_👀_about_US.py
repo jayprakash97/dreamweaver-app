a3416ef149932e725c8c42344e0632cfe1676866
Jay Prakash <59421807+jayprakash97@users.noreply.github.com>
2024-09-30 01:59:53 -0700

    Update 00_👀_about_US.py

--- pages/00_👀_about_US.py
+++ pages/00_👀_about_US.py
@@ -1,5 +1,12 @@
 import streamlit as st
 
+col1, col2  = st.columns(2, vertical_alignment="center")
+# col1, col2 = st.columns(2, horizontal_alignment="left")
+with col1:
+    st.image("pages/WS_Logo.png", width=200)
+with col2:
+    st.write("")
+  
 st.title(" Welcome to WonderScribe")
 st.write(
 """
