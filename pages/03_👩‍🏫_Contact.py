b07fcb9ff31999b5a2eeac6d38df759a2f910f0c
Jay Prakash <59421807+jayprakash97@users.noreply.github.com>
2024-09-29 17:38:58 -0700

    Update 03_👩‍🏫_Contact.py

--- pages/03_👩‍🏫_Contact.py
+++ pages/03_👩‍🏫_Contact.py
@@ -7,12 +7,10 @@ with st.form("form_key"):
     Department = st.selectbox("Department", options=["Sales", "Human Resources", "Information Technology", "Public Relation", "Complaince", "Marketing", "Data Engineering", "Data Science", "Data Analytics"])
     Full_name = st.text_input("Enter your full name. (First Name, Middle Name, Last Name)")
     Company = st.text_input("Enter your Company Name")
-    audience = st.selectbox("Audience", options=["children", "young adult", "adult", "senior"])
-    story_setting = st.selectbox("Story Setthing", options=["Magical Kingdoms", "Underwater Kingdoms", "Pirate ships", "Exotic locations", "Imaginary world", "Digital words", "Others"])
-    story_type = st.selectbox("Story Type", options=["Fantacy", "Fairy Tales", "Mythology", "Bedtime stories", "Adventure", "Mystery", "Love", "Horror", ])
-    story_theme = st.text_input("What would be topic of the story?", placeholder="Leave brief idea of a story")
-    moral_lesson = st.text_input("What would be the moral of this story?")
-    story_length = st.selectbox("Story Length (in words) ", options=["300", "400", "500"])
-    
+    Country = st.text_input("Enter your Country Name")
+    State = st.text_input("Enter your State or Province Name")
+    email = st.text_input("Enter your email address")
+    phone = st.text_input("Enter your phone# (country code, city code, and local phone number)")
+    question = st.text_input("Question or Comments")
     submit_btn = st.form_submit_button("Submit")
 
