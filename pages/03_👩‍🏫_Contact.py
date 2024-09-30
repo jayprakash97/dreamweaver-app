e86ca5d3bd7ac5f710802acb6ad0df624ee26486
Jay Prakash <59421807+jayprakash97@users.noreply.github.com>
2024-09-29 17:39:59 -0700

    Update 03_👩‍🏫_Contact.py

--- pages/03_👩‍🏫_Contact.py
+++ pages/03_👩‍🏫_Contact.py
@@ -3,7 +3,6 @@ import streamlit as st
 st.title("Contact with WonderScriber Team")
 
 with st.form("form_key"):
-    st.write("Craft personalized stories that bring adventure to life and ignite imagination and creativity")
     Department = st.selectbox("Department", options=["Sales", "Human Resources", "Information Technology", "Public Relation", "Complaince", "Marketing", "Data Engineering", "Data Science", "Data Analytics"])
     Full_name = st.text_input("Enter your full name. (First Name, Middle Name, Last Name)")
     Company = st.text_input("Enter your Company Name")
@@ -11,6 +10,6 @@ with st.form("form_key"):
     State = st.text_input("Enter your State or Province Name")
     email = st.text_input("Enter your email address")
     phone = st.text_input("Enter your phone# (country code, city code, and local phone number)")
-    question = st.text_input("Question or Comments")
+    question = st.text_area("Question or Comments")
     submit_btn = st.form_submit_button("Submit")
 
