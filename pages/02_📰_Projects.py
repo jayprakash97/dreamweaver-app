9e34d09b7fa16446bdf782f4be428dfcef144f72
Jay Prakash <59421807+jayprakash97@users.noreply.github.com>
2024-09-30 02:23:42 -0700

    Update 02_📰_Projects.py

--- pages/02_📰_Projects.py
+++ pages/02_📰_Projects.py
@@ -19,3 +19,14 @@ def bleu_scores(predicated_response, gold_response):
   bleu = evaluate.load('bleu')
   bleu_scores = bleu.compute(predictions=predicated_response, references=gold_response)
   return bleu_scores
+
+def bert_scores(predicated_response, gold_response):
+  import evaluate
+  bertscore = evaluate.load('bertscore')
+  bert_scores = bertscore.compute(predictions=predicated_response, references=gold_response, lang= 'en')
+  bert_scores['precision'] = sum(bert_scores['precision']) / len(bert_scores['precision'])
+  bert_scores['recall'] = sum(bert_scores['recall']) / len(bert_scores['recall'])
+  bert_scores['f1'] = sum(bert_scores['f1']) / len(bert_scores['f1'])
+  return bert_scores
+
+
