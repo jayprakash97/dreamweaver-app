f58ea135368775b93ad22234f1b74b8d0fb16e2e
Jay Prakash <59421807+jayprakash97@users.noreply.github.com>
2024-09-30 01:27:34 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,16 +6,15 @@ from io import BytesIO
 from PIL import Image
 
 st.set_page_config(page_title="Multiple App", page_icon="👌")
-col1, col2  = st.columns(2, vertical_alignment="center")
 print(st.__version__)
+col1, col2  = st.columns(2, vertical_alignment="center")
 # col1, col2 = st.columns(2, horizontal_alignment="left")
 with col1:
     st.image("pages/WS_Logo.png", width=100)
    
 
-
 # Streamlit app title
-st.title("Welcome to WonderScribe Page", font_size="20px")
+st.title("Welcome to WonderScribe Page", font_size="20")
 
  
 # # Create input fields to collect data for the POST request body
