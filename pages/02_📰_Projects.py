40ca719dc85913ff4727768692020486137833c1
Jay Prakash <59421807+jayprakash97@users.noreply.github.com>
2024-09-30 02:12:47 -0700

    Update 02_📰_Projects.py

--- pages/02_📰_Projects.py
+++ pages/02_📰_Projects.py
@@ -1,1 +1,20 @@
+import streamlit as st
 
+validation_query_response = {
+  0: {"query": "jay 300",
+      "gold_response": " "}
+  1: {"query": "Mike 400",
+      "gold_response": " "}
+
+# Function to evaluate RAG system using ROUGE
+def rouge_scores(predicated_response, gold_response):
+  import evaluate
+  rouge = evaluate.load('rouge')
+  rouge_scores = rouge.compute(predictions=predicated_response, references=gold_response)
+  return rouge_scores
+
+def bleu_scores(predicated_response, gold_response):
+  import evaluate
+  bleu = evaluate.load('bleu')
+  bleu_scores = bleu.compute(predictions=predicated_response, references=gold_response)
+  return bleu_scores
