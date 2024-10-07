cd33af4a11c817f3e8b98263c6adde976ba712fc
Jay Prakash <59421807+jayprakash97@users.noreply.github.com>
2024-10-06 17:43:39 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,43 +28,10 @@ st.title("Welcome to WonderScribe Page")
 # name = st.text_input("Enter your name")
 # age = st.number_input("Enter your age", min_value=0)
 
-#========== Function for evaluating RAG application ===========
-validation_query_response_list = {
-  0: {'query': 'Female Srila children Magical Kingdoms Fantacy Brushing the tooth Develop hygiene habbits 300',
-      'gold_response': 'In the enchanting Magical Kingdoms, where unicorns roamed and fairies danced, lived a young girl named Srila. Srilas days were filled with wonder and adventure, but there was one task she often neglected - brushing her teeth. One morning, as Srila was exploring the enchanted forest, she stumbled upon a curious creature. It was a tiny tooth fairy, with delicate wings and a sparkling wand. The tooth fairy fluttered around Srila, sprinkling a magical dust on her teeth. "What is this?" Srila asked, feeling a tingle in her mouth. "This is a special tooth-cleaning dust," the fairy explained. "It will keep your teeth strong and shiny, but you must remember to brush them every day."Srila promised to be more diligent about her dental hygiene. The tooth fairy smiled and vanished in a shower of glitter. From that day on, Srila made brushing her teeth a daily ritual. She used a soft-bristled brush and sweet-tasting toothpaste infused with fairy magic. Srila would sing songs and dance as she brushed, making it a fun and enjoyable activity. As Srilas teeth grew healthier and brighter, she noticed other changes too. Her breath stayed fresh, and she could enjoy all her favorite foods without any discomfort. The tooth fairy would visit occasionally, praising Srilas dedication and rewarding her with small treasures. '},
-  1: {'query': 'Mike 400',
-      'gold_response': ' '}
-}
-st.write("1")
-st.write(f"validation_query_response.: {validation_query_response_list[0]}")
-# Function to evaluate RAG system using ROUGE
-def rouge_scores(predicated_response, gold_response):
-  import evaluate
-  rouge = evaluate.load('rouge')
-  rouge_scores = rouge.compute(predictions=predicated_response, references=gold_response)
-  return rouge_scores
-
-def bleu_scores(predicated_response, gold_response):
-  import evaluate
-  bleu = evaluate.load('bleu')
-  bleu_scores = bleu.compute(predictions=predicated_response, references=gold_response)
-  return bleu_scores
-
-def bert_scores(predicated_response, gold_response):
-  import evaluate
-  bertscore = evaluate.load('bertscore')
-  bert_scores = bertscore.compute(predictions=predicated_response, references=gold_response, lang= 'en')
-  bert_scores['precision'] = sum(bert_scores['precision']) / len(bert_scores['precision'])
-  bert_scores['recall'] = sum(bert_scores['recall']) / len(bert_scores['recall'])
-  bert_scores['f1'] = sum(bert_scores['f1']) / len(bert_scores['f1'])
-  return bert_scores
-
 
 def image_decode(image_data_decode):
     image_data = base64.b64decode(image_data_decode)
     return image.open(BytesIO(image_data))
-
-
 
 with st.form("form_key"):
     st.write("Craft personalized stories that bring adventure to life and ignite imagination and creativity")
@@ -161,21 +128,6 @@ if submit_btn:  # st.button("Submit"):
             # image1 = image_decode(body_content["image_data_decode"])
             # st.image(image1, caption='Decoded Image', use_column_width=True)
 
-      #       for 1 in validation_query_response_list.keys():
-      #           references_answer.append(validation_questions_answers[i]['gold_answer'])
-      #           if validation_questions_answers[i]['gold_answer'] =        "story_type" : story_type,
-      #  "main_character" : main_character,
-      #  "story_theme" : story_theme, # 'Brushing the tooth',
-      #  "moral_lesson" : moral_lesson,
-      #  "setting" : story_setting, 
-      #  "word_count" : story_length
-      #           model_output = 
-      #           0: {'query': 'Female Srila children Magical Kingdoms Fantacy Brushing the tooth Develop hygiene habbits 300',
-      # 'gold_response': 'In the enchanting Magical Kingdoms, where unicorns roamed and fairies danced, lived a young girl named Srila. Srila's days were filled with wonder and adventure, but there was one task she often neglected - brushing her teeth. One morning, as Srila was exploring the enchanted forest, she stumbled upon a curious creature. It was a tiny tooth fairy, with delicate wings and a sparkling wand. The tooth fairy fluttered around Srila, sprinkling a magical dust on her teeth. "What's this?" Srila asked, feeling a tingle in her mouth. "This is a special tooth-cleaning dust," the fairy explained. "It will keep your teeth strong and shiny, but you must remember to brush them every day."Srila promised to be more diligent about her dental hygiene. The tooth fairy smiled and vanished in a shower of glitter. From that day on, Srila made brushing her teeth a daily ritual. She used a soft-bristled brush and sweet-tasting toothpaste infused with fairy magic. Srila would sing songs and dance as she brushed, making it a fun and enjoyable activity. As Srila's teeth grew healthier and brighter, she noticed other changes too. Her breath stayed fresh, and she could enjoy all her favorite foods without any discomfort. The tooth fairy would visit occasionally, praising Srila's dedication and rewarding her with small treasures. '},
-  
-            model_input = gender + main_character + audience + story_setting + story_type + story_theme + moral_lession + story_length
-            st.text(f"model_input: {model_input}")
-            #       base64_string1 = body_content["image_data_decode1"]
             image_data1 = base64.b64decode(base64_string1)
             image1 = Image.open(BytesIO(image_data1))
             st.image(image1, caption='Decoded Image1', use_column_width=True)
