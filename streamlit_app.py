f5b080bdb7671eb6993b5f7ce9a1f178edf4015e
Jay Prakash <59421807+jayprakash97@users.noreply.github.com>
2024-10-15 08:22:36 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,8 +22,6 @@ with col2:
 # st.title("Welcome to WonderScribe Page", font_size="20px")
 st.title("Welcome to WonderScribe Page")
 
-  
- 
 # # Create input fields to collect data for the POST request body
 # name = st.text_input("Enter your name")
 # age = st.number_input("Enter your age", min_value=0)
@@ -31,6 +29,44 @@ st.title("Welcome to WonderScribe Page")
 def image_decode(image_data_decode):
     image_data = base64.b64decode(image_data_decode)
     return Image.open(BytesIO(image_data))
+
+def fetch_story_data(payload):
+    # This function takes the payload ( useriput) and return story_text and captions
+    AWS_API_URL = "https://wacnqhon34.execute-api.us-east-1.amazonaws.com/dev/"
+    headers = {
+        "Content-Type": "application/json"
+    }
+    
+    requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+    response = requests.post(AWS_API_URL, headers=headers, json=payload, verify=False)
+   
+    if response.status_code == 200:
+        data = response.json()
+        return data["story_texts"], data["captions"]
+    else:
+        return [], []
+ 
+ 
+def fetch_and_decode_images(captions):
+    # This function takes caption of an story as an input and return encoded image data array
+    AWS_API_URL = "https://wacnqhon34.execute-api.us-east-1.amazonaws.com/dev/"
+    headers = {
+        "Content-Type": "application/json"
+    }
+   
+    decoded_images = []
+    for caption in captions:
+        payload2 = {
+            "apiPath" : 'getImage',
+            "storyPrompt" : caption
+        }  
+        json_data = payload2
+        requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+        response = requests.post(AWS_API_URL, headers=headers, json=json_data, verify=False)
+        if response.status_code == 200:
+            data = response.json()
+            decoded_images.append(data["image_data_decode1"])
+    return decoded_images
 
 with st.form("form_key"):
     st.write("Craft personalized stories that bring adventure to life and ignite imagination and creativity")
@@ -75,47 +111,31 @@ if submit_btn:  # st.button("Submit"):
        "story_theme" : story_theme, # 'Brushing the tooth',
        "moral_lesson" : moral_lesson,
        "setting" : story_setting, 
-       "word_count" : story_length
+       "word_count" : story_length,
+        "api_Path" : "getStory"
       }
     # Create the payload (data) to be sent in the POST request
 
-      
-    try:
-        json_data = payload
-        # Make a POST request to the AWS API
-        response = requests.post(AWS_API_URL, headers=headers, json=json_data)
-        # st.write("response....", response.json())
-        # Check if the request was successful (status code 200-299)
-        if response.status_code == 200:
-            data = response.json()  # Parse JSON response
-            # st.success("POST request successful!")
-            # st.write("data......", data)
-            # Extract the body content, which is a JSON string itself
-            # body_content = json.loads(data["body"])
-            # st.write("Response from API:", body_content)
-            # Extract the story text
-            # story_text = body_content["text"]
-            story_text = data["text"]
-            st.title("Children's Story")
-            # st.write(story_text)
+    story_texts, captions = fetch_story_data(payload)
+    decoded_images = fetch_and_decode_images(captions)
 
-            image1 = image_decode(data["image_data_decode1"])
-            # st.image(image1, caption='Decoded Image', use_column_width=True)
+    image1 = image_decode(decoded_images[0])
+    # st.image(image1, caption='Decoded Image', use_column_width=True)
             
-            image2 = image_decode(data["image_data_decode2"])
-            # st.image(image2, caption='Decoded Image', use_column_width=True)
+    image2 = image_decode(decoded_images[1])
+    # st.image(image2, caption='Decoded Image', use_column_width=True)
 
-            image3 = image_decode(data["image_data_decode3"])
-            # st.image(image3, caption='Decoded Image', use_column_width=True)
+    image3 = image_decode(decoded_images[2])
+    # st.image(image3, caption='Decoded Image', use_column_width=True)
             
-            image4 = image_decode(data["image_data_decode4"])
-            # st.image(image4, caption='Decoded Image', use_column_width=True)
+    image4 = image_decode(decoded_images[3])
+    # st.image(image4, caption='Decoded Image', use_column_width=True)
 
-            image5 = image_decode(data["image_data_decode5"])
+    image5 = image_decode(decoded_images[4])
   
-            st.markdown("""
-                    <style>
-                    .storybook {
+    st.markdown("""
+        <style>
+            .storybook {
                         font-family: 'Courier New', Courier, monospace;
                         background-color: #f9f5ec;
                         padding: 50px;
@@ -140,52 +160,34 @@ if submit_btn:  # st.button("Submit"):
                         display: flex;
                         justify-content: space-between;
                     }
-                    </style>
-            """, unsafe_allow_html=True)
+        </style>
+    """, unsafe_allow_html=True)
          
-            # Title for the storybook
-            st.title("📖 My Storybook")
+    # Title for the storybook
+    st.title("📖 My Storybook")
 
-            pattern = r'Panel \d+\s+Description:\s*(.*?)\s+Text:\s*(.*?)(?=Captions:|Panel|\Z)'
-             
-            matches = re.findall(pattern, story_text, re.DOTALL)
-             
-            descriptions = []
-            texts = []
-             
-            # Extracting the results into lists
-            for description, panel_text in matches:
-                descriptions.append(description.strip())
-                texts.append(panel_text.strip())
-             
-            col1, col2, col3, col4, col5 = st.columns(5)
-            
-            # with col1:
-            #     st.markdown(f'<div class="storybook"><p>{texts[0]}</p></div>', unsafe_allow_html=True)
-            #     st.image(image1, caption=descriptions[0], use_column_width=True)
-            # with col1:
-            st.markdown(f'<div class="storybook"><p>{texts[0]}</p></div>', unsafe_allow_html=True)
-            st.image(image1, caption=descriptions[0], use_column_width=True)
-            st.markdown(f'<div class="storybook"><p>{texts[1]}</p></div>', unsafe_allow_html=True)
-            st.image(image2, caption=descriptions[1], use_column_width=True)
-
-            with col2:
-                st.markdown(f'<div class="storybook"><p>{texts[1]}</p></div>', unsafe_allow_html=True)
-                st.image(image2, caption=descriptions[1], use_column_width=True)
-            with col3:
-                st.markdown(f'<div class="storybook"><p>{texts[2]}</p></div>', unsafe_allow_html=True)
-                st.image(image3, caption=descriptions[2], use_column_width=True)
-            with col4:
-                st.markdown(f'<div class="storybook"><p>{texts[3]}</p></div>', unsafe_allow_html=True)
-                st.image(image4, caption=descriptions[3], use_column_width=True)
-            with col5:
-                st.markdown(f'<div class="storybook"><p>{texts[4]}</p></div>', unsafe_allow_html=True)
-                st.image(image5, caption=descriptions[4], use_column_width=True)
-        else:
-            st.error(f"Failed with status code: {response.status_code}")
-            st.write(response.text)  # Display the error message from API
-    except Exception as e:
-        st.error(f"An error occurred: {str(e)}")
+    col1, col2, col3, col4, col5 = st.columns(5)
+        
+    with col1:
+        st.markdown(f'<div class="storybook"><p>{story_texts[0]}</p></div>', unsafe_allow_html=True)
+        st.image(image1, caption=captions[0], use_column_width=True)
+    with col2:
+        st.markdown(f'<div class="storybook"><p>{story_texts[1]}</p></div>', unsafe_allow_html=True)
+        st.image(image2, caption=captions[1], use_column_width=True)
+    with col3:
+        st.markdown(f'<div class="storybook"><p>{story_texts[2]}</p></div>', unsafe_allow_html=True)
+        st.image(image3, caption=captions[2], use_column_width=True)
+    with col4:
+        st.markdown(f'<div class="storybook"><p>{story_texts[3]}</p></div>', unsafe_allow_html=True)
+        st.image(image4, caption=captions[3], use_column_width=True)
+    with col5:
+        st.markdown(f'<div class="storybook"><p>{story_texts[4]}</p></div>', unsafe_allow_html=True)
+        st.image(image5, caption=captions[4], use_column_width=True)
+    else:
+        st.error(f"Failed with status code: {response.status_code}")
+        st.write(response.text)  # Display the error message from API
+except Exception as e:
+    st.error(f"An error occurred: {str(e)}")
 
 st.sidebar.success("Select a page above.")
 st.sidebar.text("Made with 💕 by WonderScribe")
