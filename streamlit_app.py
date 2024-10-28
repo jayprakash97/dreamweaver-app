8b28791b46d55d9ceceff5b016308e0029708266
Jay Prakash <59421807+jayprakash97@users.noreply.github.com>
2024-10-28 10:53:42 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,9 +13,11 @@ import boto3
 def get_aws_credentials():
     # Create a Secrets Manager client
     client = boto3.client("secretsmanager", region_name="us-east-1")
+    
     # Retrieve the secret
     response = client.get_secret_value(SecretId="jaypeeidsecretcode")
     # response = client.get_secret_value(SecretId=st.secrets["streamlit_s3_credentials"])
+    
     secret_dict = json.loads(response["SecretString"])
     return secret_dict["AWS_ACCESS_KEY_ID"], secret_dict["AWS_SECRET_ACCESS_KEY"]
  
